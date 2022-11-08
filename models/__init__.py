6199c74458cf99844db570ae4563bc301cca2fc5
wang.weiran <wang.wei.ran@outlook.com>
2022-11-08 16:47:41 +1100

    add Resource

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base, init_db
 from .sys_user import SysUser, SysRole, SysPermission
+from .resources import Resource
 from .relations import UserRole, RolePermission
---
+++ models/resources.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from sqlalchemy import Integer, Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from . import Base
+
+
+class Resource(Base):
+    __tablename__ = 'resource'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), comment="title name")
+    url = Column(String(255), unique=True, comment="unique url")
+
+    created_time = Column(DateTime,
+                          default=datetime.now,
+                          comment="create time")
+
+    modified_time = Column(DateTime,
+                           default=datetime.now,
+                           onupdate=datetime.now,
+                           comment="update time")
+
+    parent_id = Column(Integer, ForeignKey('resource.id'))
+    sub_resource = relationship("resource", backref="resource")
+
+    type = Column(String(50))
+    __mapper_args__ = {
+        'polymorphic_identity': 'resource',
+        'polymorphic_on': type
+    }
+
+
+class Content(Resource):
+    __tablename__ = 'content'
+    id = Column(Integer, ForeignKey('resource.id'), primary_key=True)
+
+    sub_title = Column(String(255), comment="post summary")
+    status = Column(String(255), comment="post summary")
+
+    parent_id = Column(Integer, ForeignKey('resource.id'))
+    sub_resource = relationship("resource", backref="resource")
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'content',
+        'inherit_condition': id == Resource.id,
+    }
