365e8bbcd1c2097c00dd44f9eea8e2b0f9688e17
wang.weiran <wang.wei.ran@outlook.com>
2022-11-08 17:09:46 +1100

    add config

---
+++ core/config.py
@@ -0,0 +1,2 @@
+class Config:
+    DB_URL = 'mariadb+mariadbconnector://root:153226@127.0.0.1:3306/fastdb?charset=utf8'
--- models/base.py
+++ models/base.py
@@ -1,13 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from core.config import Config
 
 Base = declarative_base()
+engine = create_engine(Config.DB_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
 
 
 def init_db():
-    db_url = 'mariadb+mariadbconnector://root:153226@127.0.0.1:3306/fastdb?charset=utf8'
-    engine = create_engine(db_url, echo=True)
     if not database_exists(engine.url):
         create_database(engine.url)
     Base.metadata.create_all(engine)
