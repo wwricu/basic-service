958a4da8f4acf3b1813584abee88c48ff1652ea1
wang.weiran <wang.wei.ran@outlook.com>
2022-11-10 03:11:11 +1100

    fix jwt

--- service/security_service.py
+++ service/security_service.py
@@ -3,17 +3,11 @@ import hashlib
 import jwt
 
 from datetime import datetime, timedelta
-
+from .config import Config
 from schemas import UserInfo
 
 
 class SecurityService:
-    __jwt_headers = {
-        "alg": "HS256",
-        "typ": "JWT"
-    }
-    __jwt_salt = "1155e364ad1e604c968bb25b0fcb9ea79b509facb4e9fc5a6f7b01905e6f963f"
-
     @staticmethod
     def generate_salt():
         return secrets.token_urlsafe()
@@ -31,20 +25,23 @@ class SecurityService:
         return password_hash == hashlib.md5(after_salt.encode(encoding='utf-8'))\
                                        .hexdigest()
 
-    @classmethod
-    def create_access_token(cls, user_info: UserInfo):
+    @staticmethod
+    def create_access_token(user_info: UserInfo):
         data = user_info.dict()
 
         data.update({"exp": datetime.utcnow() + timedelta(minutes=60)})
         return jwt.encode(payload=data,
-                          key=cls.__jwt_salt,
+                          key=Config.jwt_secret,
                           algorithm='HS256',
-                          headers=cls.__jwt_headers)
+                          headers={
+                              "alg": "HS256",
+                              "typ": "JWT"
+                          })
 
-    @classmethod
-    def verify_token(cls, token: str):
+    @staticmethod
+    def verify_token(token: str):
         data = jwt.decode(token,
-                          cls.__jwt_salt,
+                          key=Config.jwt_secret,
                           algorithms=['HS256'])
 
         if data['exp'] > datetime.utcnow():
