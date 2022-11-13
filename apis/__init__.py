55e991c3f592a8cc1d203609dffbcd07e0ebb5c7
wang.weiran <wang.wei.ran@outlook.com>
2022-11-14 07:23:18 +1100

    rename folder api

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 
 from .user_controller import user_router
 from .auth_controller import auth_router
-from .folder_controller import content_router
+from .folder_controller import folder_router
 
 router = APIRouter()
 router.include_router(user_router)
 router.include_router(auth_router)
-router.include_router(content_router)
+router.include_router(folder_router)
--- apis/folder_controller.py
+++ apis/folder_controller.py
@@ -5,16 +5,16 @@ from schemas import FolderInput, FolderOutput
 from service import ResourceService
 
 
-content_router = APIRouter(prefix="/folder")
+folder_router = APIRouter(prefix="/folder")
 
 
-@content_router.post("", response_model=FolderOutput)
+@folder_router.post("", response_model=FolderOutput)
 async def add_folder(folder: FolderInput):
     return ResourceService.add_resource(Folder(title=folder.title,
                                                parent_id=folder.parent_id))
 
 
-@content_router.get("",  response_model=list[FolderOutput])
+@folder_router.get("",  response_model=list[FolderOutput])
 async def get_folder(folder_id: int = None,
                      url: str = None,
                      parent_id: int = None):
@@ -23,13 +23,13 @@ async def get_folder(folder_id: int = None,
                                                  parent_id=parent_id))
 
 
-@content_router.put("", response_model=FolderOutput)
+@folder_router.put("", response_model=FolderOutput)
 async def modify_folder(folder: FolderInput):
     return ResourceService.modify_resource(Folder(id=folder.id,
                                                   title=folder.title,
                                                   parent_id=folder.parent_id))
 
 
-@content_router.delete("/{folder_id}", response_model=int)
+@folder_router.delete("/{folder_id}", response_model=int)
 async def delete_folder(folder_id: int):
     return ResourceService.remove_resource(Folder(id=folder_id))
