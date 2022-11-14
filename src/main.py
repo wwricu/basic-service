673511dc07f989586b38abc04257ce4a51123149
wang.weiran <wang.wei.ran@outlook.com>
2022-11-14 12:10:04 +1100

    allow credentials

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=False,
+    allow_origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
