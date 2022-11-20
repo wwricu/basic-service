235ff2029ad58397cbdff399fed6a7e1317c3d62
wang.weiran <wang.wei.ran@outlook.com>
2022-11-20 12:08:24 +1100

    optimize dao, add insert, delete all

--- src/dao/base_dao.py
+++ src/dao/base_dao.py
@@ -1,72 +1,66 @@
-from core.decorator import alchemy_session
-from copy import copy
-
-
 class BaseDao:
     @staticmethod
+    def get_query(obj, class_name, db):
+        res = db.query(class_name)
+
+        for key in class_name.__dict__:
+            if key[0] == '_' or not hasattr(obj, key):
+                continue
+            attr = getattr(obj, key)
+            if attr is None:
+                continue
+            if isinstance(attr, int) or isinstance(attr, str):
+                res = res.filter(getattr(class_name, key) == attr)
+        return res
+
+    @staticmethod
     def insert(obj, db):
-        try:
-            db.add(obj)
-            db.commit()
-            db.refresh(obj)
-            db.expunge(obj)
-            return obj
-        finally:
-            db.close()
+        db.add(obj)
+        db.commit()
+        # db.refresh(obj)
+        # db.expunge(obj)
+        return obj
 
     @staticmethod
     def insert_all(obj, db):
-        try:
-            db.add_all(obj)
-            db.commit()
-        finally:
-            db.close()
+        db.add_all(obj)
+        db.commit()
 
     @staticmethod
     def select(obj, class_name, db):
-        try:
-            res = db.query(class_name)
-
-            for key in class_name.__dict__:
-                if key[0] == '_' or not hasattr(obj, key):
-                    continue
-                attr = getattr(obj, key)
-                if attr is None:
-                    continue
-                if isinstance(attr, int) or isinstance(attr, str):
-                    res = res.filter(getattr(class_name, key) == attr)
-
-            return res.all()
-        finally:
-            db.close()
+        res = BaseDao.get_query(obj, class_name, db).all()
+        db.commit()
+        return res
 
     @staticmethod
     def update(obj, class_name, db):
         if obj.id is None or obj.id == 0:
             return
 
-        try:
-            origin_obj = db.query(class_name) \
-                           .filter_by(id=obj.id).one()
-            for key in obj.__dict__:
-                if key[0] == '_' or getattr(obj, key) is None:
-                    continue
-                # if key != '_sa_instance_state':
-                setattr(origin_obj, key, getattr(obj, key))
-            db.commit()
-            return copy(origin_obj)
-        finally:
-            db.close()
+        origin_obj = db.query(class_name) \
+                       .filter_by(id=obj.id).one()
+        for key in obj.__dict__:
+            if key[0] == '_' or getattr(obj, key) is None:
+                continue
+            # if key != '_sa_instance_state':
+            setattr(origin_obj, key, getattr(obj, key))
+        db.commit()
+        return origin_obj
 
     @staticmethod
     def delete(obj, class_name, db):
         if obj.id is None or obj.id == 0:
             return
-        try:
-            count = db.query(class_name) \
-                      .filter_by(id=obj.id) \
-                      .delete()
-            db.commit()
-            return count
-        finally:
-            db.close()
+        count = db.query(class_name) \
+                  .filter_by(id=obj.id) \
+                  .delete()
+        db.commit()
+        return count
+
+    @staticmethod
+    def delete_all(objs, class_name, db):
+        count = 0
+        for obj in objs:
+            count += BaseDao.get_query(obj, class_name, db).delete()
+        db.commit()
+        return count
