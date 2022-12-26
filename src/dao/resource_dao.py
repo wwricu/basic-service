a8fae87c750d30c844c765630d5d82256149d11f
wang.weiran <wang.wei.ran@outlook.com>
2022-12-27 07:11:36 +1100

    use join to filter post

--- src/dao/resource_dao.py
+++ src/dao/resource_dao.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from models import Content, PostTag
+from models import Content, PostCategory, PostTag
 
 
 class ResourceDao:
@@ -15,10 +15,10 @@ class ResourceDao:
 
         if parent_url is not None and len(parent_url) > 0:
             res = res.filter(obj_class.parent_url == parent_url)
+        if category_name is not None:
+            res = res.join(PostCategory).filter(PostCategory.name == category_name)
         if tag_name is not None:
             res = res.filter(obj_class.tags.any(PostTag.name == tag_name))
-        if category_name is not None:
-            res = res.filter(obj_class.category.name == category_name)
         if page_size != 0:
             # res = res.offset(page_idx * page_size).limit(page_size)
             res = res.slice(page_idx * page_size, (page_idx + 1) * page_size)
@@ -36,7 +36,7 @@ class ResourceDao:
         if parent_url is not None and len(parent_url) > 0:
             res = res.filter(obj_class.parent_url == parent_url)
         if category_name is not None:
-            res = res.filter(obj_class.category.name == category_name)
+            res = res.join(PostCategory).filter(PostCategory.name == category_name)
         if tag_name is not None:
             res = res.filter(obj_class.tags.any(PostTag.name == tag_name))
         db.commit()
