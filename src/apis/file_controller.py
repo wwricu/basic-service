43f4798ee5ceb74a02db1b2850185a85d5346186
wang.weiran <wang.wei.ran@outlook.com>
2023-01-17 23:33:19 +1100

    optimize file upload

--- src/apis/file_controller.py
+++ src/apis/file_controller.py
@@ -10,16 +10,11 @@ file_router = APIRouter(prefix="/file", tags=["file"])
 @file_router.post("/static/content",
                   dependencies=[Depends(RequiresRoles('admin'))])
 async def upload(files: list[UploadFile], request: Request):
-    err_files, succ_files = [], []
-    content_id = request.headers['x-content-id']
+    succ_files, content_id = [], request.headers['x-content-id']
     if content_id is None:
         raise HTTPException(status_code=404, detail='no content id')
 
-    content_root_path = 'static/content'
-    if not os.path.exists(content_root_path):
-        os.makedirs(content_root_path)
-
-    content_path = f'{content_root_path}/{content_id}'
+    content_path = f'static/content/{content_id}'
     if not os.path.exists(content_path):
         os.makedirs(content_path)
 
@@ -36,14 +31,5 @@ async def upload(files: list[UploadFile], request: Request):
                 'name': file.filename,
                 'path': path
             })
-        else:
-            err_files.append({file.filename})
 
-    return {
-        'msg': 'success',
-        'code': 0,
-        'data': {
-            'errFiles': err_files,
-            'succFiles': succ_files
-        }
-    }
+    return {'files': succ_files}
