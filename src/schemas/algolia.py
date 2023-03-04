58d7fc5d72e89aeebecd46feca5ee05d8841ae25
wang.weiran <wang.wei.ran@outlook.com>
2023-03-04 15:42:15 +1100

    do not upload content

--- src/schemas/algolia.py
+++ src/schemas/algolia.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import base64
 from datetime import datetime
 
 from pydantic import BaseModel
@@ -12,9 +11,7 @@ class AlgoliaPostIndex(BaseModel):
     title: str = ''
     category: str = ''
     tags: list[str] = []
-    content: str = ''
     # unsearchable attrs below
-    relative_url: str
     created_time: int = 0
     updated_time: int = 0
 
@@ -26,11 +23,7 @@ class AlgoliaPostIndex(BaseModel):
             title=content.title,
             created_time=int(datetime.timestamp(content.created_time)),
             updated_time=int(datetime.timestamp(content.updated_time)),
-            relative_url=f'content/{content.id}'
         )
-        if content.content is not None:
-            index.content = base64.b64decode(content.content).decode()
-        # must open database session in apis
         if content.category_id is not None:
             index.category = content.category.name
         if content.tags is not None:
--- test/service_test/algolia_service.py
+++ test/service_test/algolia_service.py
@@ -1,10 +1,14 @@
 import asyncio
 from typing import Awaitable
 
+import aiohttp
 from algoliasearch.search_client import SearchClient
+
 
 app_id = ''
 api_key = ''
+search_key = ''  # search key
+index_name = 'blog_content_index'
 
 
 async def delete_non_exist():
@@ -17,7 +21,26 @@ async def delete_non_exist():
         print(results)
 
 
-async def main():
+async def restful_search(keyword: str):
+    url = f'https://{app_id}-dsn.algolia.net/1/indexes/{index_name}/query'
+    headers = {
+        'X-Algolia-Application-Id': app_id,
+        'X-Algolia-API-Key': search_key,
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = {"params": 'query=log'}
+    async with aiohttp.ClientSession() as client:
+        async with client.post(
+            url=url,
+            headers=headers,
+            json=data,
+        ) as response:
+            if response.status != 200:
+                print('failed')
+            print(await response.json())
+
+
+async def save_object():
     async with SearchClient.create(app_id, api_key) as client:
         index = client.init_index('dev_blog_post')
 
@@ -31,4 +54,8 @@ async def main():
             results = await results
         print(results)
 
-asyncio.run(delete_non_exist())
+
+async def main():
+    await restful_search('l')
+
+asyncio.run(main())
