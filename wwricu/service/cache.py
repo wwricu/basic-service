6f46547b8317eb23976e6ea9ef8f4a4c3c051df7
wwr <me@wwr.icu>
2025-01-06 21:39:41 +0800

    [feat] trace coroutine

--- wwricu/service/cache.py
+++ wwricu/service/cache.py
@@ -1,24 +1,28 @@
 import asyncio
 
 
-async def cache_delete(key: str):
-    if key in cache_data:
-        del cache_data[key]
-
-
 async def timeout(key: str, second: int):
     await asyncio.sleep(second)
     await cache_delete(key)
 
 
-async def cache_set(key: str, value: any, second: int = 600):
+async def cache_delete(key: str):
+    cache_data.pop(key)
+    if task := timeout_callback.pop(key, None):
+        task.cancel()
+
+
+async def cache_set(key: str, value: any, second: int | None = 600):
+    if task := timeout_callback.pop(key, None):
+        task.cancel()
     cache_data[key] = value
-    if second > 0:
-        asyncio.create_task(timeout(key, second))
+    if second is not None and second > 0:
+        timeout_callback[key] = asyncio.create_task(timeout(key, second))
 
 
 async def cache_get(key: str) -> any:
     return cache_data.get(key)
 
 
-cache_data: dict = dict()
+cache_data: dict[str, any] = dict()
+timeout_callback: dict[str, asyncio.Task] = dict()
