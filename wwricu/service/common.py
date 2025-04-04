a00c13318d215378416f61f29450f23814e46dd6
wwr <me@wwr.icu>
2025-04-04 17:31:30 +0800

    [feat] add scheduler

--- wwricu/service/common.py
+++ wwricu/service/common.py
@@ -3,22 +3,29 @@ import datetime
 import time
 from contextlib import asynccontextmanager
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI
 from loguru import logger as log
 from sqlalchemy import select, func, delete
 
-from wwricu.domain.entity import BlogPost, EntityRelation, PostTag
+from wwricu.domain.entity import BlogPost, EntityRelation, PostTag, PostResource
 from wwricu.domain.enum import CacheKeyEnum, PostStatusEnum, TagTypeEnum, RelationTypeEnum
 from wwricu.config import Config
 from wwricu.service.cache import cache
 from wwricu.service.category import reset_category_count
 from wwricu.service.database import engine, get_session, new_session
+from wwricu.service.storage import oss
 from wwricu.service.tag import reset_tag_count
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    scheduler = AsyncIOScheduler()
     try:
+        scheduler.add_job(hard_delete_expiration, trigger=CronTrigger(hour=3, minute=0, second=0))
+        scheduler.add_job(clean_post_resource, trigger=CronTrigger(day=1, hour=3, minute=0, second=0))
+        scheduler.start()
         await reset_tag_count()
         await reset_category_count()
         await reset_system_count()
@@ -26,6 +33,7 @@ async def lifespan(_: FastAPI):
         log.info(f'listening on {Config.host}:{Config.port}')
         yield
     finally:
+        scheduler.shutdown()
         await cache.close()
         await engine.dispose()
         log.info('Exit')
@@ -69,6 +77,7 @@ async def update_system_count():
 
 
 async def hard_delete_expiration():
+    log.info('hard deleting expired entities')
     async with new_session() as s:
         deadline = datetime.datetime.now() - datetime.timedelta(days=Config.trash_expire_day)
         stmt = select(BlogPost).where(BlogPost.deleted == True).where(BlogPost.update_time < deadline)
@@ -110,3 +119,16 @@ async def hard_delete_expiration():
         # delete categories
         result = await s.execute(stmt)
         log.info(f'{result.rowcount} categories deleted')
+
+
+async def clean_post_resource():
+    """delete all unused files from oss"""
+    # TODO: soft delete not resources not referenced by posts
+    log.info('start cleaning post resources')
+    async with new_session() as s:
+        resource_keys = await s.scalars(select(PostResource.key))
+        resource_keys = set(resource_keys.all())
+        all_s3_objects = oss.list_all()
+        keys_to_del = list(filter(lambda key: key not in resource_keys, map(lambda r: r.Key, all_s3_objects)))
+        log.warning(f'{len(keys_to_del)} objects to be deleted')
+        oss.batch_delete(keys_to_del)
