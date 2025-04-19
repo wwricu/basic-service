c97afeead4c4bb724234c00594e2af3081b629ec
wwr <me@wwr.icu>
2025-04-19 17:20:57 +0800

    [feat] remove varchar length

--- wwricu/domain/entity.py
+++ wwricu/domain/entity.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from sqlalchemy import Boolean, DateTime, Integer, String, TEXT, UniqueConstraint, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-from wwricu.domain.constant import EntityConstant
 from wwricu.domain.enum import PostStatusEnum, RelationTypeEnum
 
 
@@ -19,15 +18,11 @@ class Base(DeclarativeBase):
 
 class BlogPost(Base):
     __tablename__ = 'wwr_blog_post'
-    title: Mapped[str] = mapped_column(String(EntityConstant.USER_STRING_LEN), default='', index=True)
+    title: Mapped[str] = mapped_column(String, default='', index=True)
     cover_id: Mapped[int] = mapped_column(Integer, nullable=True)
     content: Mapped[str] = mapped_column(TEXT, default='')
     preview: Mapped[str] = mapped_column(TEXT, default='')
-    status: Mapped[str] = mapped_column(
-        String(EntityConstant.ENUM_STRING_LEN),
-        default=PostStatusEnum.DRAFT.value,
-        index=True
-    )
+    status: Mapped[str] = mapped_column(String, default=PostStatusEnum.DRAFT.value, index=True)
     category_id: Mapped[int] = mapped_column(Integer, nullable=True)
 
 
@@ -39,8 +34,8 @@ class PostTag(Base):
     """
     __tablename__ = 'wwr_post_tag'
     __table_args__ = (UniqueConstraint('name', 'type', name='uix_name_type'),)
-    name: Mapped[str] = mapped_column(String(EntityConstant.USER_STRING_LEN))
-    type: Mapped[str] = mapped_column(String(EntityConstant.ENUM_STRING_LEN))
+    name: Mapped[str] = mapped_column(String)
+    type: Mapped[str] = mapped_column(String)
     count: Mapped[int] = mapped_column(Integer, default=0)
 
 
@@ -48,22 +43,19 @@ class EntityRelation(Base):
     __tablename__ = 'wwr_entity_relation'
     src_id: Mapped[int] = mapped_column(Integer, index=True)
     dst_id: Mapped[int] = mapped_column(Integer, index=True)
-    type: Mapped[RelationTypeEnum] = mapped_column(
-        String(EntityConstant.ENUM_STRING_LEN),
-        default=RelationTypeEnum.POST_TAG
-    )
+    type: Mapped[RelationTypeEnum] = mapped_column(String, default=RelationTypeEnum.POST_TAG)
 
 
 class PostResource(Base):
     __tablename__ = 'wwr_post_resource'
     post_id: Mapped[int] = mapped_column(Integer, index=True)
-    name: Mapped[str] = mapped_column(String(EntityConstant.USER_STRING_LEN), nullable=True)
-    key: Mapped[str] = mapped_column(String(EntityConstant.LONG_STRING_LEN))
-    type: Mapped[str] = mapped_column(String(EntityConstant.ENUM_STRING_LEN), nullable=True)
+    name: Mapped[str] = mapped_column(String, nullable=True)
+    key: Mapped[str] = mapped_column(String)
+    type: Mapped[str] = mapped_column(String, nullable=True)
     url: Mapped[str] = mapped_column(TEXT, nullable=True)
 
 
 class SysConfig(Base):
     __tablename__ = 'wwr_sys_config'
-    key: Mapped[str] = mapped_column(String(EntityConstant.USER_STRING_LEN), nullable=False, unique=True)
+    key: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     value: Mapped[str] = mapped_column(TEXT, nullable=True)
