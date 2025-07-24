eeab61908ea6cb4bf1ae332e1b205bf5e25d3564
wwr <me@wwr.icu>
2025-07-24 22:39:20 +0800

    [feat] change global dependency position

--- wwricu/api/__init__.py
+++ wwricu/api/__init__.py
@@ -1,14 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from wwricu.api.common import common_api
 from wwricu.api.manage import manage_api
 from wwricu.api.open import open_api
 from wwricu.api.post import post_api
 from wwricu.api.tag import tag_api
-from wwricu.service.database import open_session
 
 
-api_router = APIRouter(dependencies=[Depends(open_session)])
+api_router = APIRouter()
 api_router.include_router(common_api)
 api_router.include_router(manage_api)
 api_router.include_router(open_api)
--- wwricu/main.py
+++ wwricu/main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from wwricu.api import api_router
 from wwricu.domain.constant import CommonConstant
 from wwricu.domain.enum import EnvVarEnum
 from wwricu.middleware import middlewares
 from wwricu.service.common import lifespan
+from wwricu.service.database import open_session
 
 
 app = FastAPI(
     title=CommonConstant.APP_NAME,
     lifespan=lifespan,
     middleware=middlewares,
+    dependencies=[Depends(open_session)],
     debug=__debug__,
     root_path=EnvVarEnum.ROOT_PATH.get()
 )
