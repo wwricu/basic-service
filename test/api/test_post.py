eaa02560f73b96c687d3340e9c98295ee98f6229
wwr <me@wwr.icu>
2026-01-22 20:38:44 +0800

    remove test warning

--- test/api/test_post.py
+++ test/api/test_post.py
@@ -7,7 +7,7 @@ from wwricu.domain.post import PostDetailVO
 from test.test_utils import client, random
 
 
-def test_create_post() -> int:
+def create_post() -> int:
     response = client.get('/post/create')
     log.info(response.json())
     assert response.status_code == status.HTTP_200_OK
@@ -15,25 +15,24 @@ def test_create_post() -> int:
     return post_detail.id
 
 
-def test_get_all_post() -> PageVO[PostDetailVO]:
+def get_all_post() -> PageVO[PostDetailVO]:
     response = client.post('/post/all', json={})
     log.info(response.json())
     assert response.status_code == status.HTTP_200_OK
     return PageVO.model_validate(response.json())
 
 
-def test_get_post_detail() -> PostDetailVO:
-    post_preview_page = test_get_all_post()
+def test_get_post_detail():
+    post_preview_page = get_all_post()
     idx = random.randint(0, len(post_preview_page.data) - 1)
     post_preview_json = post_preview_page.data[idx]
     post_preview = PostDetailVO.model_validate(post_preview_json)
     response = client.get(f'/post/detail/{post_preview.id}')
-    log.info(response.json())
-    return PostDetailVO.model_validate(response.json())
+    log.info(PostDetailVO.model_validate(response.json()))
 
 
-def test_delete_post() -> int:
-    post_id = test_create_post()
+def test_delete_post():
+    post_id = create_post()
     response = client.get(f'/post/delete/{post_id}')
     assert response.status_code == status.HTTP_200_OK
-    return post_id
+    log.info(f'{post_id=}')
