613bdbe5f55d9b6cff0d70ad4adbc97cf6afbb14
wwr <me@wwr.icu>
2026-01-22 20:27:40 +0800

    remove unique index

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,15 @@
 from random import Random
 
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
 
+from wwricu.domain.entity import Base
 from wwricu.main import app
+
+
+def show_ddl():
+    sync_engine = create_engine('sqlite:///:memory:', echo=True)
+    Base.metadata.create_all(sync_engine)
 
 
 client = TestClient(app)
--- wwricu/domain/entity.py
+++ wwricu/domain/entity.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, DateTime, Integer, String, TEXT, UniqueConstraint, func
+from sqlalchemy import Boolean, DateTime, Integer, String, TEXT, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from wwricu.domain.enum import PostStatusEnum, RelationTypeEnum
@@ -25,30 +25,28 @@ class BlogPost(Base):
     content: Mapped[str] = mapped_column(TEXT, default='')
     preview: Mapped[str] = mapped_column(TEXT, default='')
     status: Mapped[str] = mapped_column(String, default=PostStatusEnum.DRAFT.value, index=True)
-    category_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    category_id: Mapped[int] = mapped_column(Integer, nullable=True, index=True)
 
 
 class PostTag(Base):
-    """
-    Why we have to separate Tag and Category: these two items are NOT same after all.
-    They need different renderings on page, they will finally need different structures and logics one day.
-    It is really unwise to store them in a same table just because of their current same structures.
-    """
     __tablename__ = 'wwr_post_tag'
-    __table_args__ = (UniqueConstraint('name', 'type', name='uix_name_type'),)
 
-    name: Mapped[str] = mapped_column(String)
-    type: Mapped[str] = mapped_column(String)
+    name: Mapped[str] = mapped_column(String, index=True)
+    type: Mapped[str] = mapped_column(String, index=True)
     count: Mapped[int] = mapped_column(Integer, default=0)
 
 
 class EntityRelation(Base):
     __tablename__ = 'wwr_entity_relation'
-    __table_args__ = (UniqueConstraint('src_id', 'dst_id', 'type', name='uix_src_id_dst_id_type'),)
 
+    '''
+    DO NOT use composite index here cause there is fixed ordinality for src_id and dst_id,
+    refer to wwricu.service.common.clean_post_resource where select with dst_id and type only;
+    while wwricu.service.common.hard_delete_expiration where select with src_id and type only
+    '''
     src_id: Mapped[int] = mapped_column(Integer, index=True)
     dst_id: Mapped[int] = mapped_column(Integer, index=True)
-    type: Mapped[RelationTypeEnum] = mapped_column(String)
+    type: Mapped[RelationTypeEnum] = mapped_column(String, index=True)
 
 
 class PostResource(Base):
@@ -56,11 +54,11 @@ class PostResource(Base):
     post_id: Mapped[int] = mapped_column(Integer, index=True)
     name: Mapped[str] = mapped_column(String, nullable=True)
     key: Mapped[str] = mapped_column(String)
-    type: Mapped[str] = mapped_column(String, nullable=True)
+    type: Mapped[str] = mapped_column(String, nullable=True, index=True)
     url: Mapped[str] = mapped_column(TEXT, nullable=True)
 
 
 class SysConfig(Base):
     __tablename__ = 'wwr_sys_config'
-    key: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    key: Mapped[str] = mapped_column(String, unique=True)
     value: Mapped[str] = mapped_column(TEXT, nullable=True)
